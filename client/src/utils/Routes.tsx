875d5f2d8602d4b4003593e3e933b7c1f842aae6
Stephen Mayhew <stephen@duolingo.com>
2020-07-29 22:09:55 -0400

    Authentication branch can be closed now

--- client/src/utils/Routes.tsx
+++ client/src/utils/Routes.tsx
@@ -1,47 +1,47 @@
-import * as React from 'react'
-import { Route, Switch } from 'react-router-dom';
-import HomePage from "../pages/HomePage";
-import LoginPage from "../pages/LoginPage";
-import { Annotate } from "../pages/Annotate";
-import PageNotFound from "../pages/PageNotFound";
-import { State } from "../utils/types"
-import { connect } from 'react-redux'
-import MainPanel from '../components/MainPanel';
+// import * as React from 'react'
+// import { Route, Switch } from 'react-router-dom';
+// import HomePage from "../pages/HomePage";
+// import LoginPage from "../pages/LoginPage";
+// import { Annotate } from "../pages/Annotate";
+// import PageNotFound from "../pages/PageNotFound";
+// import { State } from "../utils/types"
+// import { connect } from 'react-redux'
+// import MainPanel from '../components/MainPanel';
 
-class Routes extends React.Component<Props> {
+// class Routes extends React.Component<Props> {
 
-  render() {
-    const { loggedIn } = this.props
-    console.log(`logged in? ${loggedIn}`);
+//   render() {
+//     const { loggedIn } = this.props
+//     console.log(`logged in? ${loggedIn}`);
 
-    return (
-      <MainPanel>
-        { loggedIn ? (
-          <Switch>
-            <Route exact={true} path="/" component={HomePage} />
-            <Route exact={true} path="/login" component={HomePage} />
-            <Route exact={true} path="/anno" component={Annotate} />
-            <Route exact={false} path="/" component={PageNotFound} />
-          </Switch>
-        ) : (
-          // TODO: consider passing the next URL here.
-          <Route exact={false} path="/" component={LoginPage} />
-        )}
-    </MainPanel>
-    );
+//     return (
+//       <MainPanel>
+//         { loggedIn ? (
+//           <Switch>
+//             <Route exact={true} path="/" component={HomePage} />
+//             <Route exact={true} path="/login" component={HomePage} />
+//             <Route exact={true} path="/anno" component={Annotate} />
+//             <Route exact={false} path="/" component={PageNotFound} />
+//           </Switch>
+//         ) : (
+//           // TODO: consider passing the next URL here.
+//           <Route exact={false} path="/" component={LoginPage} />
+//         )}
+//     </MainPanel>
+//     );
     
-  }
-}
+//   }
+// }
 
-type Props = {
-  loggedIn: boolean
-}
+// type Props = {
+//   loggedIn: boolean
+// }
 
-const mapStateToProps = (state: State) => ({
-  loggedIn: state.loggedIn
-})
+// const mapStateToProps = (state: State) => ({
+//   loggedIn: state.loggedIn
+// })
 
-const mapDispatchToProps = (dispatch: any) => ({
-})
+// const mapDispatchToProps = (dispatch: any) => ({
+// })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Routes)
+// export default connect(mapStateToProps, mapDispatchToProps)(Routes)
