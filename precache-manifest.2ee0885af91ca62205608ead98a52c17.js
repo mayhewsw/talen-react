116917e0d78d7eb180faf5ab6e3bf51a8022edec
Stephen Mayhew <stephen@duolingo.com>
2020-09-03 21:26:47 -0400

    Updates

---
+++ precache-manifest.2ee0885af91ca62205608ead98a52c17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf113b3b42ea78e362ea0f5d90433fc",
+    "url": "/talen-react/index.html"
+  },
+  {
+    "revision": "27cd23ed35125041d62f",
+    "url": "/talen-react/static/css/2.25d8efc1.chunk.css"
+  },
+  {
+    "revision": "a61efff4cb7cc9d16bb6",
+    "url": "/talen-react/static/css/main.1131ed16.chunk.css"
+  },
+  {
+    "revision": "27cd23ed35125041d62f",
+    "url": "/talen-react/static/js/2.582a6fad.chunk.js"
+  },
+  {
+    "revision": "fc24e97800533712fbe1a3c6add8543a",
+    "url": "/talen-react/static/js/2.582a6fad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a61efff4cb7cc9d16bb6",
+    "url": "/talen-react/static/js/main.c2aa7c61.chunk.js"
+  },
+  {
+    "revision": "640e2fbf7f2374a37827",
+    "url": "/talen-react/static/js/runtime-main.26ee70fd.js"
+  }
+]);
