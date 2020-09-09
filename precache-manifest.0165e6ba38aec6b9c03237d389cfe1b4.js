e0544bc9bedfac8907c881c65778743deb1bde8d
Stephen Mayhew <stephen@duolingo.com>
2020-09-08 22:17:04 -0400

    Updates

---
+++ precache-manifest.0165e6ba38aec6b9c03237d389cfe1b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13363b9af8c19dc97ba0cfe9a3876c8c",
+    "url": "/talen-react/index.html"
+  },
+  {
+    "revision": "6939011e3e79e0d7b475",
+    "url": "/talen-react/static/css/2.25d8efc1.chunk.css"
+  },
+  {
+    "revision": "9ce0d7a1eb1efa931ca3",
+    "url": "/talen-react/static/css/main.d8b4a202.chunk.css"
+  },
+  {
+    "revision": "6939011e3e79e0d7b475",
+    "url": "/talen-react/static/js/2.76f19645.chunk.js"
+  },
+  {
+    "revision": "fc24e97800533712fbe1a3c6add8543a",
+    "url": "/talen-react/static/js/2.76f19645.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ce0d7a1eb1efa931ca3",
+    "url": "/talen-react/static/js/main.17dda7b3.chunk.js"
+  },
+  {
+    "revision": "640e2fbf7f2374a37827",
+    "url": "/talen-react/static/js/runtime-main.26ee70fd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "talen-react/precache-manifest.6cd6dc5d2b779565277af06f32519ca7.js"
+  "/talen-react/precache-manifest.0165e6ba38aec6b9c03237d389cfe1b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("talen-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/talen-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
