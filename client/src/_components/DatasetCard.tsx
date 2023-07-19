669ff7a9a592da1508c21f43ad73808744cc0ac4
Stephen Mayhew <swm.mayhew@gmail.com>
2023-07-18 21:39:08 -0400

    Oops, not just English

--- client/src/_components/DatasetCard.tsx
+++ client/src/_components/DatasetCard.tsx
@@ -6,6 +6,51 @@ import { connect } from "react-redux";
 
 class DatasetCard extends React.Component<Props> {
   render() {
+    const language_map: any = {
+      en: "English",
+      es: "Spanish",
+      de: "German",
+      fr: "French",
+      it: "Italian",
+      nl: "Dutch",
+      pt: "Portuguese",
+      ru: "Russian",
+      zh: "Chinese",
+      ar: "Arabic",
+      ja: "Japanese",
+      ko: "Korean",
+      he: "Hebrew",
+      tr: "Turkish",
+      vi: "Vietnamese",
+      id: "Indonesian",
+      fa: "Persian",
+      hi: "Hindi",
+      ur: "Urdu",
+      ta: "Tamil",
+      th: "Thai",
+      sw: "Swahili",
+      sv: "Swedish",
+      sk: "Slovak",
+      yo: "Yoruba",
+      tl: "Tagalog",
+      fi: "Finnish",
+      hu: "Hungarian",
+      cs: "Czech",
+      pl: "Polish",
+      bg: "Bulgarian",
+      ro: "Romanian",
+      da: "Danish",
+      no: "Norwegian",
+      el: "Greek",
+      ceb: "Cebuano",
+      mr: "Marathi",
+    };
+
+    // const language = language_map[this.props.dataset_id.split("_")[0]]
+    const short_language = this.props.dataset_id.split("_")[0];
+    const language = language_map[short_language];
+    const dataset_name = this.props.dataset_id.split("_")[1].toUpperCase();
+
     const progress = this.props.datasetStats
       ? Math.floor(
           (100 * this.props.datasetStats.numAnnotated) /
@@ -42,9 +87,9 @@ class DatasetCard extends React.Component<Props> {
                     }
                     onClick={() =>
                       this.props.saveToGithub({
-                        repo_name: `UNER_English-${this.props.dataset_id
-                          .split("_")[1]
-                          .toUpperCase()}`,
+                        // dataset id looks like: en_ewt
+                        // language_map maps en to English
+                        repo_name: `UNER_${language}-${dataset_name}`,
                         dataset_key: this.props.split_id,
                       })
                     }
