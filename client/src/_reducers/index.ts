4c3bcedc4be9d3a6fd52345e0ef06f1711143d2e
Stephen Mayhew <swm.mayhew@gmail.com>
2026-01-11 14:22:22 -0500

    Finished phases 1 and 2

--- client/src/_reducers/index.ts
+++ client/src/_reducers/index.ts
@@ -1,14 +1,10 @@
 import { combineReducers } from "redux";
 
-import { authentication } from "./authentication.reducer";
-import { registration } from "./registration.reducer";
 import { data } from "./data.reducer";
 import { alert } from "./alert.reducer";
 import { util } from "./util.reducer";
 
 const rootReducer = combineReducers({
-  authentication,
-  registration,
   data,
   alert,
   util,
